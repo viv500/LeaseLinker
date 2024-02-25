2ab580e2b57f94cd5da11557b81b9cc07686c24d
viv500 <vivek.j.manoj@gmail.com>
2024-02-25 03:07:30 -0500

    ok bye bye

--- src/pages/GetStartedPage/GetStartedPage.tsx
+++ src/pages/GetStartedPage/GetStartedPage.tsx
@@ -4,7 +4,7 @@ import GetStartedNavBar from "./GetStartedNavBar";
 
 function GetStartedPage() {
   return (
-    <div className="bg-white relative flex min-h-lvh justify-center items-center flex-col">
+    <div className="relative flex min-h-lvh justify-center items-center flex-col">
       <GetStartedNavBar />
 
       <div className=" justify-center items-center gap-10 max-w-7xl flex p-4 flex-wrap-reverse grow">
